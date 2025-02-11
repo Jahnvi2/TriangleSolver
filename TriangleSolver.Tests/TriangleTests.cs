c864ca6e15ca0635908a39319c17e69ce425d643
Jahnvi2 <166559155+Jahnvi2@users.noreply.github.com>
2025-02-11 18:54:53 -0500

    Added unit tests for Isosceles Triangle cases

--- TriangleSolver.Tests/TriangleTests.cs
+++ TriangleSolver.Tests/TriangleTests.cs
@@ -20,8 +20,36 @@ namespace TriangleSolver.Tests
             Assert.AreEqual("Equilateral triangle", result);
         }
 
+        [Test]
+        public void AnalyzeTriangle_IsoscelesTriangle_TwoEqualSides1_ReturnsIsosceles()
+        {
+            // Arrange
+            int side1 = 5, side2 = 5, side3 = 7;
 
-        
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.AreEqual("Isosceles triangle", result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_IsoscelesTriangle_TwoEqualSides2_ReturnsIsosceles()
+        {
+            int side1 = 6, side2 = 8, side3 = 8;
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.AreEqual("Isosceles triangle", result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_IsoscelesTriangle_TwoEqualSides3_ReturnsIsosceles()
+        {
+            int side1 = 9, side2 = 9, side3 = 5;
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.AreEqual("Isosceles triangle", result);
+        }
+
+
 
     }
 }
