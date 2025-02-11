e767b615500831e65d06159d114d696ae5d4c6f4
Jahnvi2 <166559155+Jahnvi2@users.noreply.github.com>
2025-02-11 18:48:27 -0500

    Added unit tests for Isosceles Triangle case

---
+++ TriangleSolver.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TriangleSolver.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriangleSolver.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35a0af6d-2c07-4430-aafc-6fc7780b23b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TriangleSolver.Tests/TriangleTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using TriangleSolver;
+
+namespace TriangleSolver.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void AnalyzeTriangle_EquilateralTriangle_ReturnsEquilateral()
+        {
+            // Arrange
+            int side1 = 5, side2 = 5, side3 = 5;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.AreEqual("Equilateral triangle", result);
+        }
+
+
+        
+
+    }
+}
